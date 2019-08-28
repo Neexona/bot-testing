e0ba81fc2ae3c50cd92fa270469fde1f21c7fabf
unknown <lima.alpha81@gmail.com>
2019-08-29 00:09:30 +0200

    Import 2

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = "*";
+const { Client } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config.js');
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === `${prefix}ping`) {
-    msg.reply('Pong!');
+  if (msg.content.startsWith(`${PREFIX}ping`)) {
+    msg.channel.send('Pong!');
   }
 });
 
-client.login('NjE2MzgwMjg1NzgwNDkyMjk4.XWbvHA.kq6x6Hf5-hxCiHGg3czM5c1hXEg');
+client.login(TOKEN);
